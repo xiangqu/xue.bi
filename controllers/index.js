40fb87baedd566545ab64c42f593cd28c8dd23e0
lxy <306766053@qq.com>
2014-05-25 17:26:13 +0800

    login

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,8 @@ module.exports = function *() {
     var User = mongoose.model('User');
     var users = yield User.findQ();
     var data = {};
+    users._id = users._id + '';
     data.users = users;
+    console.log(data);
     yield this.render('index', data);
 };
